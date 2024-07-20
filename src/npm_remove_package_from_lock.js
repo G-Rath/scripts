4a0c6e27d7fd34207f78fc599717c930c0bd3f3b
Gareth Jones <jones258@gmail.com>
2024-07-21 11:15:56 +1200

    fix(npm_remove_package_from_lock): support all lockfile versions

--- src/npm_remove_package_from_lock.js
+++ src/npm_remove_package_from_lock.js
@@ -22,10 +22,48 @@ fs.writeFileSync(
   JSON.stringify(
     packageLock,
     (key, value) => {
-      if (packagesToRemove.includes(key) && typeof value === 'object') {
-        removedCount++;
+      if (typeof value === 'object') {
+        // handles lockfile v2+ format
+        if (key === 'packages') {
+          return Object.fromEntries(
+            Object.entries(value).filter(([key]) => {
+              if (
+                packagesToRemove.some(
+                  pkg =>
+                    key.endsWith(`node_modules/${pkg}`) ||
+                    key === `node_modules/${pkg}`
+                )
+              ) {
+                removedCount++;
 
-        return undefined;
+                return false;
+              }
+
+              // remove the dependency if its a child of a package to remove
+              // just without counting it as a removed package
+              return !packagesToRemove.some(pkg =>
+                key.includes(`node_modules/${pkg}/`)
+              );
+            })
+          );
+        }
+
+        // handles lockfile v1 format
+        if (key === 'dependencies') {
+          return Object.fromEntries(
+            Object.entries(value).filter(([key, v]) => {
+              // lockfile v2+ has a "dependencies" object with string values
+              // which specifies the contraints, so we don't want remove that
+              if (typeof v === 'object' && packagesToRemove.includes(key)) {
+                removedCount++;
+
+                return false;
+              }
+
+              return true;
+            })
+          );
+        }
       }
 
       return value;
